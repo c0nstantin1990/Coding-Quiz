49fe34c6852f0244acda7fc6163318dd5c8ed629
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-05-05 10:17:57 -0400

    asigned js variables to html elements

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var startButton = document.getElementById("start");
+var quizContainer = document.getElementById(".container");
+var questionElement = document.getElementById("question");
+var choicesElement = document.getElementById("choices");
+var submitButton = document.getElementById("submit");
+var timerElement = document.getElementById("time");
+var scoreElement = document.getElementById("points");
